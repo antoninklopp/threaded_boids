e873b4fd1f0abbed0e93f21527b73edb508bec31
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-02-19 11:09:54 +0100

    Refactoring to compile the project

---
+++ 01_cpp_threading/bin/CMakeFiles/boids_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boids_cpp.dir/src/Boid.cpp.o"
+  "CMakeFiles/boids_cpp.dir/src/Flock.cpp.o"
+  "CMakeFiles/boids_cpp.dir/src/Vector2D.cpp.o"
+  "libboids_cpp.pdb"
+  "libboids_cpp.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boids_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01_cpp_threading/bin/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/antonin/Documents/threaded_boids/01_cpp_threading/main.cpp" "/home/antonin/Documents/threaded_boids/01_cpp_threading/bin/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/antonin/Documents/threaded_boids/01_cpp_threading/bin/CMakeFiles/boids_cpp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
