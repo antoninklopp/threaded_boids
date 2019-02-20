4f989974f9e84b1982ac59742e4f539a66c44976
Antonin <antonin.klopp-tosser@grenoble-inp.org>
2019-02-20 14:46:21 +0100

    Added measures for 01

--- 01_cpp_threading/main.cpp
+++ 01_cpp_threading/main.cpp
@@ -8,8 +8,15 @@
 #include "Flock.hpp"
 #include "Common.hpp"
 
+#include <cstdlib>
+
 vector<Flock> flocks;
 int nb_of_flocks;
+
+const int NUMBER_MEASURES=10;
+static int measure = 0;  
+static float _time = 0; 
+static int nb_flocks = 0; 
 
 void updateEverything() {
 	vector<std::thread> flock_threads;
@@ -41,8 +48,15 @@ void updateEverything() {
 
 	elapsed = (finish.tv_sec - start.tv_sec);
 	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+	_time += elapsed; 
 	
-	std::cerr << "time taken " << elapsed << endl;
+	measure ++; 
+
+	if (measure == 20){
+		_time /=20; 
+		std::cout << nb_flocks <<  " " << _time << endl; 
+		exit(0); 
+	}
 
 }
 
@@ -57,12 +71,12 @@ int main(int argc, char *argv[]) {
 	glutCreateWindow("Boids");
 	glutDisplayFunc(updateEverything);
 		
-
 	// Initialize Positions
 
 	// vector<Flock> flocks;
 
-	nb_of_flocks = 100;
+	nb_of_flocks = atoi(argv[1]);
+	nb_flocks = nb_of_flocks; 
 
 	for (int i = 0; i < nb_of_flocks; i++) {
 		Flock flock(colors[i]);
